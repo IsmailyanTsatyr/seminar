6ef5c53552702255e8b65d6adfde6dcc11e0c4db
Tsatyr Ismailyan <palint1@mail.ru>
2023-03-07 23:53:42 +0300

    сделал дз

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Console.Write("Введите первое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number < number2)
+      Console.WriteLine(number2); 
+else 
+      Console.WriteLine(number);  
+
+*/
+/*
+Console.Write("Введите первое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число "
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье  число ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if (number>max) max = number;
+if (number2>max) max = number2;
+if (number3>max) max = number3;
+
+Console.WriteLine(max);
+*/
+/*
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+while(current <= number)
+{
+    Console.Write(current + " ");
+    current +=2;
+
+}
+*/ 
+
+
+
